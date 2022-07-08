0be31102df25c3ae95bc96b4dd2f14aebce7ce73
Zamudio Alejandro <ale-zamudio2011@hotmail.com>
2022-07-08 14:29:38 -0300

    1er etapa commit

---
+++ operador.js
@@ -0,0 +1,11 @@
+const dia = "martes";
+switch(dia){
+case "lunes":
+    console.log("El dia pertenece al switch");
+    break;
+case "miercoles":
+    console.log("El dia pertenece al switch");
+    break;
+default:
+    console.log("El dia NO pertenece al switch");     
+}
